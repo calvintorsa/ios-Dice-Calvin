d8cce5f033b8c9aa55180f47b1be68198f55b072
Turnip Bill Saur <7314076@student.unpar.ac.idgit config --global user.name calvintorsa>
2018-08-21 15:52:12 +0700

    commit

--- Dice Calvin/ViewController.swift
+++ Dice Calvin/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDice1: Int=0
+    var randomDice2: Int=0
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var daduSatu: UIImageView!
+    @IBOutlet weak var daduDua: UIImageView!
+    
+    @IBAction func acakButton(_ sender: UIButton) {
+        randomDice1 = Int(arc4random_uniform(6))
+        randomDice2 = Int(arc4random_uniform(6))
+      
+        
+        daduSatu.image = UIImage(named: diceArray[randomDice1])
+        daduDua.image = UIImage(named: diceArray[randomDice2])
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
 }
 
